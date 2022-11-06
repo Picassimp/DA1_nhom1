80b5cd3bf3a263faa591399b1295357cd5e0a905
raikojima <duyvo761@gmail.com>
2022-11-06 22:11:20 +0700

    thêm HomeActivity, bottomnavmenu và home fragment

---
+++ app/src/main/java/com/example/da1/Views/Fragments/HomeFragment.java
@@ -0,0 +1,48 @@
+package com.example.da1.Views.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.da1.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
