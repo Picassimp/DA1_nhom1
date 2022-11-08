5eca432d03e9b18916617e1ba22127bf1c5337ae
raikojima <duyvo761@gmail.com>
2022-11-08 23:37:35 +0700

    thêm activity thực hiện làm quiz và tạo fragment cho quiz (Duy)

---
+++ app/src/main/java/com/example/da1/Views/Fragments/quiz_singlechoice_fragment.java
@@ -0,0 +1,52 @@
+package com.example.da1.Views.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.da1.R;
+
+
+public class quiz_singlechoice_fragment extends Fragment {
+
+
+    public quiz_singlechoice_fragment() {
+        // Required empty public constructor
+    }
+
+
+//    public static quiz_singlechoice_fragment newInstance(String param1, String param2) {
+//        quiz_singlechoice_fragment fragment = new quiz_singlechoice_fragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_singlechoice_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
